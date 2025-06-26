6da50b0b470bc3d5bd2668d0b6b8f85d928b7ed2
Ankit Kumar <ankit1842kumar@gmail.com>
2025-06-27 02:06:40 +0530

    worked nav responsive

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,10 +6,11 @@ export const AppContext = createContext();
 export const AppContextProvider = ({children})=>{
 
     const navigate = useNavigate()
-    const [user,setUser] = useState(null);
+    const [user,setUser] = useState(true);
     const [isSeller,setIsSeller] = useState(false)
+    const [showUserLogin,setShowUserLogin] = useState(false)
 
-    const value = {}
+    const value = {navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin}
     return <AppContext.Provider value={value}>
         {children}
 
